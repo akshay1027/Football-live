461af40112ad87ec94d6ece93e10a6bdd3eb0b98
Akshay.R.R <65683151+akshay1027@users.noreply.github.com>
2020-10-24 22:46:15 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+ //getting the DOM elements
+var elapsedTime = document.querySelector("#elapsed");
+var homeTeamImage = document.querySelector("#homeLogo");
+var homeTeamName = document.querySelector("#homeName");
+
+ 
+var awayTeamImage = document.querySelector("#awayLogo");
+
+ 
+var awayTeamName = document.querySelector("#awayName");
+var lastMatchGoal = document.querySelector("#goals");
+var matchTable = document.querySelector("#matchTable");
